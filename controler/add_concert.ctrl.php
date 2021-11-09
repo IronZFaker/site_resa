f5125a0caceded87aef5ce3ec40d4b18fbf20b50
Thomas <thomas.plazas.reau@gmail.com>
2021-11-09 11:02:42 +0100

    test

--- controler/add_concert.ctrl.php
+++ controler/add_concert.ctrl.php
@@ -1,18 +1,19 @@
 <?php
-    echo "before";
-    include_once("../model/DAO.class.php");
-    $dao = new DAO();
+echo "before";
+include_once("../model/DAO.class.php");
+echo "aftetr include";
+$dao = new DAO();
+echo "aftetr new DAO";
 
-    $id = $_GET['id'];
-    $nom = $_GET['nom'];
-    $date = $_GET['date'];
-    $zone1 = $_GET['zone1'];
-    $zone2 = $_GET['zone2'];
-    $zone3 = $_GET['zone3'];
+$id = $_GET['id'];
+$nom = $_GET['nom'];
+$date = $_GET['date'];
+$zone1 = $_GET['zone1'];
+$zone2 = $_GET['zone2'];
+$zone3 = $_GET['zone3'];
 
-    var_dump($date);
 
-    // $dao->addConcert($id, $nom, $date, $zone1, $zone2, $zone3);
-    echo "after";
-    // include("../view/accueil.view.php");
+// $dao->addConcert($id, $nom, $date, $zone1, $zone2, $zone3);
+echo "after";
+// include("../view/accueil.view.php");
 ?>
--- model/DAO.class.php
+++ model/DAO.class.php
@@ -1,9 +1,11 @@
 <?php
+    echo "in dao class";
     require_once("../model/Panier.class.php");
     require_once("../model/Place.class.php");
     require_once("../model/Zone.class.php");
     require_once("../model/Siege.class.php");
     require_once("../model/Concert.class.php");
+    echo "in dao class after require once";
     // Le Data Access Object
     // Il représente la base de donnée
     class DAO {
