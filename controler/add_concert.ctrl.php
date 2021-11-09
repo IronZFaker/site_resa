4e1db9ac54e03bfe7b817b1e3d3fa92bcb06144d
Thomas <thomas.plazas.reau@gmail.com>
2021-11-09 10:08:49 +0100

    ctrl add concert

--- controler/add_concert.ctrl.php
+++ controler/add_concert.ctrl.php
@@ -1,4 +1,15 @@
 <?php
     include_once("../model/DAO.class.php");
     $dao = new DAO();
+
+    $id = $_GET['id'];
+    $nom = $_GET['nom'];
+    $date = $_GET['date'];
+    $zone1 = $_GET['zone1'];
+    $zone2 = $_GET['zone2'];
+    $zone3 = $_GET['zone3'];
+
+    $dao->addConcert($id, $nom, $date, $zone1, $zone2, $zone3);
+
+    include("../view/accueil.view.php");
 ?>
--- view/add_concert.view.php
+++ view/add_concert.view.php
@@ -5,7 +5,10 @@
         <title></title>
     </head>
     <body>
-        <form action="../view/concert.view" method="get">
+        <form action="../controler/add_concert.ctrl.php" method="get">
+          <div>
+            <input type="number hidden" name="id" id="id" value="-1">
+          </div>
           <div>
             <label for="nom">Nom du concert : </label>
             <input type="text" name="nom" id="nom" required>
@@ -15,14 +18,14 @@
             <input type="date" name="date" id="date" required>
           </div>
           <div>
-            <label for="nom">Tarif zone 1 : </label>
+            <label for="zone1">Tarif zone 1 : </label>
             <input type="number" id="zone1" name="zone1" min="1" max="1000">
             <br>
-            <label for="nom">Tarif zone 2 : </label>
+            <label for="zone2">Tarif zone 2 : </label>
             <input type="number" id="zone2" name="zone2" min="1" max="1000">
             <br>
-            <label for="nom">Tarif zone 3 : </label>
-            <input type="number" id="zone2" name="zone2" min="1" max="1000">
+            <label for="zone3">Tarif zone 3 : </label>
+            <input type="number" id="zone3" name="zone3" min="1" max="1000">
           </div>
           <div>
             <input type="submit" value="submit">
